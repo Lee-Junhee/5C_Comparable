0d667d33243b76e1841db5d24071220dd1a5cdf4
Lee-Junhee <jlee02@stuy.edu>
2019-04-07 23:01:13 -0400

    implemented comparable in Date

--- Date.java
+++ Date.java
@@ -3,7 +3,18 @@
  */
 public class Date{
     private int y,m,d;
-    
+   
+    public int compareTo(Object otherObj)
+        Date otherDate = (Date) otherObj;
+	int tempResult;
+        if ((tempResult = Integer.valueOf(y).compareTo(
+			  Integer.valueOf(otherDate.y))) != 0)
+	    return tempResult;
+	if ((tempResult = Integer.valueOf(m).compareTo(
+		          Integer.valueOf(otherDate.m))) != 0)
+	    return tempResult;
+	return Integer.valueOf(d).compareTo(
+	       Integer.valueOf(otherDate.d));
 
     // -------- previously-written code --------
     // constructor
